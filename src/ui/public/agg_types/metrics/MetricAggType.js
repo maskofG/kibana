ef95fe8892b78b66c47bfe090add622d949891c2
Pierre Padovani <ppadovani@homeaway.com>
2017-02-13 10:08:51 -0600

    Backport fixes

--- src/ui/public/agg_types/metrics/MetricAggType.js
+++ src/ui/public/agg_types/metrics/MetricAggType.js
@@ -27,7 +27,7 @@ define(function (require) {
 
       // Return proper values when no buckets are present
       // `Count` handles empty sets properly
-      if (!bucket[agg.id] && isSettableToZero) return 0;
+      if ((!bucket[agg.id] && !bucket['nested_' + agg.id]) && isSettableToZero) return 0;
 
       return (bucket[agg.id] || bucket['nested_' + agg.id][agg.id]).value;
     };
